afe2f553dd68904b93d9cfcd942202247c1fe3b8
ndsapa2020 <ndsapa2020@gmail.com>
2020-07-25 18:08:46 +0600

    human

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,45 @@
 import java.io.File;
-import java.util.logging.FileHandler;
 
 public class Main {
     public static void main(String[] args) {
         String folderPath = "D://Media";
         File file  = new File(folderPath);
 
-        System.out.println(file.length());
+
+       long size = getFolderSize(file);
+        System.out.println(getHumanReadable(size));
+
+    }
+    public static long getFolderSize (File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
+
+    }
+
+    public static String getHumanReadable(long size) {
+       long curr = size/1024;
+       String suff = "KB";
+       if (curr > 1024) {
+           suff = "MB";
+           curr = curr / 1024;
+           if (curr > 1024) {
+               suff = "GB";
+               curr = curr / 1024;
+               if (curr > 1024) {
+                   suff = "TB";
+                   curr = curr / 1024;
+               }
+           }
+       }
+
+        return curr + suff;
+
     }
 }
