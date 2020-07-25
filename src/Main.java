f2c7f983148c0cbb521d3f1105b6bbe7b9b47b61
ndsapa2020 <ndsapa2020@gmail.com>
2020-07-25 16:20:53 +0600

    Git integreted to IDEA

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 import java.io.File;
-import java.util.logging.FileHandler;
 
 public class Main {
     public static void main(String[] args) {
         String folderPath = "D://Media";
         File file  = new File(folderPath);
 
-        System.out.println(file.length());
+        System.out.println(getFolderSize(file));
+    }
+    public static long getFolderSize (File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
